bd150b55c5aa86ce676c50ac7c3e1259b8f2675d
Igwe0001 <nwobodoemmanuel479@gmail.com>
2022-10-19 10:40:28 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import icon from "./assets/icon-dice.svg";
+import desktop from "./assets/pattern-divider-desktop.svg";
+import mobile from "./assets/pattern-divider-mobile.svg";
 
 function App() {
+  const [advice, setAdvice] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchAdviceHandler = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice", {
+        cache: "no-cache",
+      });
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const data = await response.json();
+
+      setAdvice(data.slip);
+      
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchAdviceHandler();
+  }, []);
+
+  let content = (
+    <p className="text-lg text-yellow-600 font-manrope text-center">
+      Found no advice.
+    </p>
+  );
+
+  if (!isLoading) {
+    content = (
+      <p className="text-xl sm:text-2xl my-8 text-center text-lightCyan font-manrope">
+        "{advice.advice}"
+      </p>
+    );
+  }
+
+  if (error) {
+    content = (
+      <p className="text-lg text-red-600 font-manrope text-center">{error}</p>
+    );
+  }
+
+  if (isLoading) {
+    content = (
+      <p className="text-lg text-blue-600 font-manrope text-center">
+        Loading...
+      </p>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="w-full min-h-[100vh] h-full grid place-items-center  bg-darkBlue">
+      <div className="max-w-[450px] w-[90%] min-h-[280px] flex items-center justify-center flex-col p-6 bg-darkGrayishBlue rounded-xl relative shadow-custom">
+        <div>
+          <h1 className="text-center text-neonGreen uppercase tracking-widest font-bold">
+            Advice <span>#{advice.id}</span>
+          </h1>
+          {content}
+        </div>
+        <div>
+          <img src={mobile} alt="second" className="sm:hidden mb-4" />
+          <img src={desktop} alt="third" className="hidden sm:block mb-4" />
+        </div>
+        <button
+          onClick={fetchAdviceHandler}
+          className="absolute bottom-[-30px] flex justify-center"
         >
-          Learn React
-        </a>
-      </header>
+          <img
+            className="block rounded-full bg-neonGreen hover:button-shadow p-4"
+            src={icon}
+            alt="first"
+          />
+        </button>
+      </div>
     </div>
   );
 }
